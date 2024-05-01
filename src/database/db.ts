33b20ba96fbf38764bb242a22043d394451f94f6
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-05-01 18:06:10 +0100

    fix: use signin instead of use

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,36 +1,34 @@
 import { Surreal } from 'surrealdb.node'
 
-import { Config } from '../config/types.js'
+import type { Config } from '../config/types.js'
 
 let db: Surreal
 
 export const getDb = () => {
-  if (db) {
-    return db
-  }
-  throw new Error('Not connected to a database')
+	if (db) {
+		return db
+	}
+	throw new Error('Not connected to a database')
 }
 
 export const connectDb = async (config: Config) => {
-  console.log('connect to database')
-  db = new Surreal()
-  await db.connect(config.surreal, {
-    ns: config.ns,
-    db: config.db,
-    auth: { user: config.username, pass: config.password },
-  })
-  await db.use({
-    ns: config.ns,
-    db: config.db,
-  })
+	console.log('connect to database')
+	db = new Surreal()
+	await db.connect(config.surreal)
+	await db.signin({
+		namespace: config.ns,
+		database: config.db,
+		username: config.username,
+		password: config.password,
+	})
 }
 
 export const insertDefinitions = async (content: string) => {
-  const db = getDb()
-  const result = await db.query(content, {})
-  console.log('insertDefinitions:', JSON.stringify(result, null, 2))
+	const db = getDb()
+	const result = await db.query(content, {})
+	console.log('insertDefinitions:', JSON.stringify(result, null, 2))
 }
 
 export const closeDb = async () => {
-  console.log('database closed')
+	console.log('database closed')
 }
