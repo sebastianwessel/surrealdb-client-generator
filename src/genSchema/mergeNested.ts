39107b53a8116a4804dbc666c2526b39df9fba75
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-07-04 01:04:09 +0100

    fix: handling of array type #29

--- src/genSchema/mergeNested.ts
+++ src/genSchema/mergeNested.ts
@@ -1,47 +1,13 @@
-import { fieldsToObject } from './fieldsToObject.js'
-import { type FieldDetail, getDetailsFromDefinition } from './getDetailsFromDefinition.js'
+import { generateZodSchemaCode } from './generateZodSchemaCode.js'
+import { getDetailsFromDefinition } from './getDetailsFromDefinition.js'
 
-export const mergeNested = (fields: Record<string, string>, isInputSchema: boolean) => {
+export const mergeNested = (fields: Record<string, string>, isInputSchema: boolean, tableName: string) => {
+	const schemaName = `${tableName}${isInputSchema ? 'Input' : 'Output'}SchemaGen`
+
 	const inputFields = Object.entries(fields)
 		.map(([_fname, definition]) => {
 			return getDetailsFromDefinition(definition, isInputSchema)
 		})
 		.filter(entry => !entry.skip)
-
-	const obj = fieldsToObject(inputFields)
-
-	const fieldMap = new Map<string, FieldDetail>()
-
-	for (const field of inputFields) {
-		fieldMap.set(field.name, field)
-	}
-
-	const mergeSchemaString = (o: object, prev?: string) => {
-		const entries = Object.entries(o)
-
-		const current = prev ? fieldMap.get(prev) : undefined
-
-		if (!entries.length) {
-			return current?.zodString ?? 'z.any()'
-		}
-
-		let res = ''
-
-		for (const [name, value] of entries) {
-			const n = prev ? `${prev}.${name}` : name
-			res += `${name}: ${mergeSchemaString(value, n)},\n`
-		}
-
-		if (current?.zodString.startsWith('z.object({})')) {
-			return current.zodString.replace('z.object({})', `z.object({${res}})`)
-		}
-
-		if (current?.zodString.startsWith('z.array(z.any())')) {
-			return current.zodString.replace('z.array(z.any())', `z.array(z.object({${res}}))`)
-		}
-
-		return `z.object({${res}})`
-	}
-
-	return mergeSchemaString(obj)
+	return generateZodSchemaCode(inputFields, schemaName)
 }
